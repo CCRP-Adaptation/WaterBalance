b1fd904cbcfd190b0090a6a28765a6210fa14f66
AKellnerDillon <anne_dillon@nps.gov>
2020-05-12 00:17:22 -0600

    Works!!

--- get_site_params.R
+++ get_site_params.R
@@ -8,12 +8,15 @@ library(sf)
 library(raster)
 library(dplyr)
 library(rgdal)
-library(ggplot2)
+library(tmap)
 #library(soilDB)
 
 ############################################################# USER INPUTS ##################################################################### 
 
-setwd("C:/Users/adillon/Documents/ArcGIS") # Set working directory to where spatial files are located
+setwd("C:/Users/adillon/Documents/ArcGIS")# Set working directory to where spatial files are located
+
+site <- "PETE"
+OutDir <- "C:/Users/adillon/Documents/RSS/PETE/"
 
 # load shapefiles for NPS park boundaries, US Counties, 
 
@@ -49,8 +52,15 @@ county$NAME[, drop = TRUE] # See console for the name of the county where the pa
 
 ######### STOP HERE AND DOWNLOAD DATA FROM https://datagateway.nrcs.usda.gov/GDGOrder.aspx #################################################
 
+# First, import DEM and soils layers into ArcGIS.
+# Join gSSURGO soils raster (MapunitRaster_10m) with valu1 table
+# Get UTM projection from DEM and reproject soils raster into appropriate projection
+# Save new raster into park folder (NOT file geodatabase)
+# NOTE: Soils raster cannot be reprojected in R because will lose data associated with RAT (Raster Attribute Table)
+
 dem <- raster('./RSS/PETE/elevation/ned30m37077.tif') # DEM 30 m downloaded from USDA NRCS
-soil <- raster('./RSS/PETE/soil') # raster file exported from ArcGIS (MapunitRaster_10m) with spatial join to valu1 table
+soil <- raster('./RSS/PETE/soil_18N') # projected raster file exported from ArcGIS (MapunitRaster_10m) with spatial join to valu1 table
+soil@data@attributes[[1]] # check that RAT looks OK
 
 # Project spatial data
 
@@ -60,14 +70,15 @@ dem_projection <- crs(dem) # raster
 park_proj <- st_transform(park, dem_projection)
 centroid_proj <- st_transform(centroid, dem_projection)
 
-# crop soil and MACA grid; reproject soil to DEM projection
+# MACA grid
+# When we can get soils data directly, crop soil grid to MACA cell and reproject to DEM
 
-maca.poly <- rasterToPolygons(maca_cell)
+maca.poly <- rasterToPolygons(maca_cell) # Create MACA polygon
 #maca.poly <- spTransform(maca.poly, soil_projection) # project MACA cell to projection of soil layer - not necessary now but will need to crop soil layer later to cut processing time for reprojection
 #soil_crop <- crop(soil, maca.poly) # crop soil raster to maca cell - same comment as above
 
-soil_proj <- projectRaster(soil, crs = dem_projection) # project cropped soil layer to DEM projection
-maca_proj <- spTransform(maca.poly, dem_projection)
+#soil_proj <- projectRaster(soil, crs = dem_projection) # project cropped soil layer to DEM projection
+maca_proj <- spTransform(maca.poly, dem_projection) # reproject MACA grid 
 
 
 #####   SLOPE, ASPECT AND RANDOM POINTS   ##########################################################################################
@@ -75,92 +86,74 @@ maca_proj <- spTransform(maca.poly, dem_projection)
 # Create slope and aspect rasters
 
 slope <- terrain(dem, opt = "slope", unit = "degrees", neighbors = 4) # 4 is better for "smooth" surfaces; 8 is better for rough. See https://www.rdocumentation.org/packages/raster/versions/3.1-5/topics/terrain
+plot(slope) # check slope looks OK
 aspect <- terrain(dem, opt = "aspect", unit = "degrees")
+plot(aspect) # check aspect looks OK
 
 # Crop to projected MACA cell 
 
+soil_crop <- crop(soil, maca_proj)
 dem_crop <- crop(dem, maca_proj)
 slope_crop <- crop(slope, maca_proj)
 aspect_crop <- crop(aspect, maca_proj)
 
-# Create random points
+# get 10 random points from soil raster and create SpatialPoints object
 
+tail(soil_crop@data@attributes)
 points <- spsample(maca_proj, n = 10, type = "random")
 
 
 ####    PLOT TO CHECK SPATIAL ALIGNMENT   ############################################################################################
 
-soil_plot <- tm_shape(soil_proj) + 
+soil_plot <- tm_shape(soil_crop) + 
   tm_raster(legend.show = FALSE) +
   tm_shape(points) +             
   tm_dots(size = 1)
 
-dem_plot <- tm_shape(dem_crop) + # because slope and aspect plot were created
+dem_plot <- tm_shape(dem_crop) + 
   tm_raster(legend.show = FALSE) + 
   tm_shape(points) + 
   tm_dots(size = 1)
 
-tmap_arrange(soil_plot, dem_plot) # make sure all points are within 
+slope_plot <- tm_shape(slope_crop) + 
+  tm_raster(legend.show = FALSE) + 
+  tm_shape(points) + 
+  tm_dots(size = 1)
 
-# plot soil raster and DEM side-by-side to make sure points fall within boundaries of both. Because cropped soil layer was created in ArcGIS, 
+aspect_plot <- tm_shape(aspect_crop) + 
+  tm_raster(legend.show = FALSE) + 
+  tm_shape(points) + 
+  tm_dots(size = 1)
 
+tmap_arrange(soil_plot, dem_plot, slope_plot, aspect_plot) # make sure all points are within boundaries and plots look good
 
-############
-set.seed(0)
-r <- raster(nrow=10, ncol=10)
-values(r) <- runif(ncell(r)) * 10
-is.factor(r)
-#is.factor(soil) # yes soil is a factor
+####    EXTRACT DATA FROM POINTS ######################################################################################################
 
-r <- round(r)
-f <- as.factor(r)
-is.factor(f)
-
-# add attribute
-x <- levels(f)[[1]] # 
-x
-x$code <- letters[10:20]
-levels(f) <- x
-levels(f)
-f
-
-r <- raster(nrow=10, ncol=10)
-values(r) = 1
-r[51:100] = 2
-r[3:6, 1:5] = 3
-r <- ratify(r) # makes values into attributes
-
-rat <- levels(r)[[1]] # list of attributes
-atts <- levels(soil)[[1]]
-
-rat$landcover <- c('Pine', 'Oak', 'Meadow')
-rat$code <- c(12,25,30)
-levels(r) <- rat
-levels(soil) <- atts # assigns attributes to levels
-r
-soil
-
-# extract values for some cells
-i <- extract(r, c(1,2, 25,100))
-
-# get 10 random points from soil raster
-
-pts <- sampleRandom(soil, size = 10)
-
-j <- extract(soil, pts)
-# get the attribute values for these cells
-factorValues(r, i)
-factorValues(soil, pts)
+is.factor(soil_crop) # should be TRUE
+#atts <- levels(soil_crop)[[1]] # list of attributes
+#levels(soil_crop) <- atts # assigns attributes to levels
+#soil_crop
+i <- extract(soil_crop, points) 
+pointSoil <- factorValues(soil_crop, i)
 
 
-pointCells <- cellFromXY(soil_proj, points)
+latlong <- st_as_sf(points) # convert to sf object 
+latlong <- st_transform(latlong, crs = 4326) # project to lat/long
 
-levels(soil)
 
-lev <- levels(soil_proj)[[1]]
-aws <- as.factor(lev$AWS0_999)
+sites <- as.data.frame(st_coordinates(latlong)) # begin new dataframe for sites
 
-i <- extract(soil_proj, pointCells)
+sites[,3] <- extract(dem, points)
+sites[,4] <- extract(aspect_crop, points)
+sites[,5] <- extract(slope_crop, points)
+sites[,6] <- pointSoil$AWS0_999
+sites[,7] <- seq.int(nrow(sites))
+  
+sites <- select(sites, 7,2,1,3:6) # reorder columns
+colnames(sites) <- c("SiteID", "Lat", "Lon", "Elev", "Aspect", "Slope", "SWC.Max")
 
-factorValues(i, aws)
-lev.factor
+write.csv(sites, file =  paste0(OutDir, site, "_site_characteristics.csv"), row.names = FALSE)
+
+
+
+
