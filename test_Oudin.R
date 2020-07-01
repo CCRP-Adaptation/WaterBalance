bcca3f6440aa9cf150547bdd5deaceb957fbd193
AKellnerDillon <anne_dillon@nps.gov>
2020-06-30 18:28:06 -0600

    Oudin equation checks out! Changed equation for declination (see master branch for detailed comment)

--- test_Oudin.R
+++ test_Oudin.R
@@ -2,22 +2,43 @@
 ##    TESTS   ########################
 ######################################
 
+rm(list=ls())
+
+# Load data
+
+ex_params <- read.csv("C:/Users/adillon/Documents/Water_Balance_Update/Example_sites.csv")
+ex_daymet <- read.csv("C:/Users/adillon/Documents/Water_Balance_Update/FortCollinsCO_daymet_R.csv")
+
+
 # ------  FUNCTIONS ------------------------------------------------------- #
 
-extraterrestrial_solar_rad = function(doy, lat){
+#' Calculates extraterrestrial Solar Radiation (MJ m^-2 day^-1) based on day-of-year and latitude
+#' @param doy Day-of-year (Julian date)
+#' @param lat Latitude (degrees)
+#' @export
+#' extraterrestrial_solar_rad()
+
+extraterrestrial_solar_rad = function(doy, lat){ #radRa - column AK
   d.r = 1 + 0.033*cos(((2*pi)/365)*doy) # cell AG9
-  declin = 0.409*sin(((2*pi)/365)*doy) # cell AH9
+  declin = 0.409*sin((((2*pi)/365)*doy)-1.39) # cell AH9
   lat.rad = (pi/180)*lat
   sunset.ang = acos(-tan(lat.rad)*tan(declin)) #cell AJ9
   R.a = ((24*60)/pi)*0.0820*d.r*(sunset.ang*sin(lat.rad)*sin(declin) + cos(lat.rad)*cos(declin)*sin(sunset.ang))
   return(R.a)
 }
 
+#' Oudin PET with radiation calculated from latitude and day-of-year
+#' Equation 3 in Oudin et al. 2010
+#' 
+#' @param x A daily time series data frame containing tmean_C (deg C)
+#' @param R.a. Extraterrestrial solar radiation
+#' @param snowpack A time series vector of snowpack accumulation values. ### ACTUALLY THIS IS SNOW WATER EQUIVALENT, MAYBE NOT SNOWPACK. LOOK INTO SNOW WATER EQUIVALENT RECONCILIATION NEXT WEEK (WRITTEN 6/17/2020)
+
 ET_Oudin = function(x, R.a., snowpack){ 
   top = R.a. * (x$tmean_C + 5) * 0.408
   bottom = 100
   PET = top/bottom
-  et.oudin = ifelse(snowpack > 2,0, ifelse(tmean_C > -5, PET, 0))
+  et.oudin = ifelse(snowpack > 2,0, ifelse(x$tmean_C > -5, PET, 0))
   return(et.oudin)
 }
 
@@ -28,3 +49,10 @@ ET_Oudin_daymet = function(x){
   et.oudin = ifelse(snowpack > 2, 0, ifelse(tmean_C > -5, PET, 0))
   return(et.oudin)
 }
+
+# --------------------------------------------------------------------------------------------------------------------------------------------- #
+R.a. <- extraterrestrial_solar_rad(ex_daymet$yday[1], ex_params$Lat)
+ex_daymet$tmean_C <- (ex_daymet$tmax..deg.c.+ex_daymet$tmin..deg.c.)/2
+ex_daymet$snowpack <- 0
+snowpack <- 0
+ET_Oudin(ex_daymet,R.a.,snowpack)
