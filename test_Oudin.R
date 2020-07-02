a9d071c314172bca6668cc2069738603c038b87b
AKellnerDillon <anne_dillon@nps.gov>
2020-07-01 18:42:14 -0600

    ENd of workday - need to model ET_Oudin equation after snowpack because of initial value issue

--- test_Oudin.R
+++ test_Oudin.R
@@ -56,3 +56,15 @@ ex_daymet$tmean_C <- (ex_daymet$tmax..deg.c.+ex_daymet$tmin..deg.c.)/2
 ex_daymet$snowpack <- 0
 snowpack <- 0
 ET_Oudin(ex_daymet,R.a.,snowpack)
+
+
+# USE THIS CODE AS AN EXAMPLE!! 
+get_snowpack = function(ppt, freeze, p.0=NULL){
+  p.i = ifelse(!is.null(p.0), p.0, 0) 
+  snowpack = c()
+  for(i in 1:length(ppt)){
+    snowpack[i] = ((1-freeze[i])^2)*ppt[i] + (1-freeze[i])*p.i
+    p.i = snowpack[i]
+  }
+  return(snowpack)
+}
