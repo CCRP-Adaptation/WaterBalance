4ace76876d7ac6268648c9a5ef52a63f77508db6
Annie Kellner <annie.kellner@colostate.edu>
2020-10-16 12:58:08 -0600

    plot MACA cell

--- get_site_params.R
+++ get_site_params.R
@@ -13,10 +13,10 @@ library(tmap)
 
 ###########################   USER INPUTS ###################################################################################### 
 
-setwd("C:/Users/msears/Documents/GIS")# Set working directory to where spatial files are located
+setwd("C:/Users/adillon/Documents/ArcGIS")# Set working directory to where spatial files are located
 
-site <- "MACA"
-OutDir <- "C:/Users/msears/Documents/RSS/Mammoth_Cave/WB/"
+site <- "PETE"
+OutDir <- "C:/Users/adillon/Documents/RSS/PETE/"
 
 # load shapefiles for NPS park boundaries, US Counties, 
 
@@ -24,17 +24,17 @@ nps_boundary <- st_read('./nps_boundary/nps_boundary.shp')
 nps_boundary_centroids <- st_read('./nps_boundary_centroids/nps_boundary_centroids.shp')
 US_Counties <- st_read('./US_Counties/tl_2016_us_county.shp')
 State_Shapefile <- st_read('./State_Shapefile/Contig_US_Albers.shp')
-maca <- raster('./Climate_grid/tdn_90d.nc') # MACA grid
+maca <- raster('Climate_grid/tdn_90d.nc') # MACA grid
+maca_proj_to_nps <- projectRaster(maca, crs = '+proj=longlat +datum=NAD83 +no_defs')
 
 # select park
 
-park <- filter(nps_boundary, UNIT_CODE == "MACA")
-centroid <- filter(nps_boundary_centroids, UNIT_CODE == "MACA")
-state <- filter(State_Shapefile, STATE_NAME == "Kentucky")
-
+park <- filter(nps_boundary, UNIT_CODE == "PETE")
+centroid <- filter(nps_boundary_centroids, UNIT_CODE == "PETE")
+state <- filter(State_Shapefile, STATE_NAME == "Virginia")
 #########################     END USER INPUTS   ##################################################################################
 
-# Check that spatial data looks OK so far. Precise rojection doesn't matter at this point but should be close. 
+# Check that spatial data looks OK so far. Precise projection doesn't matter at this point but should be close. 
 
 state_and_park <- tm_shape(state) +
   tm_borders() + 
@@ -59,6 +59,18 @@ centroid.sp <- SpatialPoints(centroid.coords) # convert centroid into SpatialPoi
 
 cell <- cellFromXY(maca, centroid.sp) # find grid cell park centroid lies within
 maca_cell <- rasterFromCells(maca, cell) # create stand-alone raster for single MACA cell
+maca.poly <- rasterToPolygons(maca_cell) # Create MACA polygon - original file in lat/long (note: datum differs from park shapefiles)
+
+# Plot to check MACA location
+
+tm_shape(park) +
+  tm_borders() +
+  tm_fill(col = "lightgreen") +
+  tm_shape(centroid) +
+  tm_dots(size = 1, shape = 3) +
+  tm_shape(maca.poly) +
+  tm_borders()
+
 
 # get county
 
@@ -72,32 +84,24 @@ county$NAME[, drop = TRUE] # See console for the name of the county where the pa
 ######### STOP HERE AND DOWNLOAD DATA FROM https://datagateway.nrcs.usda.gov/GDGOrder.aspx #################################################
 
 # First, import DEM and soils layers into ArcGIS.
-# Join gSSURGO soils raster (MapunitRaster_10m) with valu1 table
-# Get UTM projection from DEM and reproject soils raster into appropriate projection
-# Save new raster into park folder (NOT file geodatabase)
-# NOTE: Soils raster cannot be reprojected in R because will lose data associated with RAT (Raster Attribute Table)
 
-dem <- raster('./RSS/MACA/Elevation/ned30m37086.tif') # DEM 30 m downloaded from USDA NRCS
-soil <- raster('./RSS/MACA/soils') # projected raster file exported from ArcGIS (MapunitRaster_10m) with spatial join to valu1 table
-soil@data@attributes[[1]] # check that RAT looks OK
+dem <- raster('C:/Users/adillon/Documents/RSS/PETE/elevation/ned30m37077.tif')  # DEM 30 m downloaded from USDA NRCS
+soil_tercek <- raster('water_storage.tif') # Mike Tercek's soil file - original projection Albert's Equal Area
 
 # Project spatial data
 
 dem_projection <- crs(dem) # raster 
-#soil_projection <- crs(soil) # This may come in handy later if we are able to import gridded soils directly into R
+soil_projection <- crs(soil_tercek) # This may come in handy later if we are able to import gridded soils directly into R
 
-park_proj <- st_transform(park, dem_projection) # reproject park
-centroid_proj <- st_transform(centroid, dem_projection) # reproject centroid
+maca.poly <- spTransform(maca.poly, soil_projection) # project MACA cell to projection of soil layer - not necessary now but will need to crop soil layer later to cut processing time for reprojection
+soil_crop <- crop(soil_tercek, maca.poly) # crop soil raster to maca cell - same comment as above
 
-# MACA grid
-# When we can get soils data directly, crop soil grid to MACA cell and reproject to DEM
 
-maca.poly <- rasterToPolygons(maca_cell) # Create MACA polygon
-#maca.poly <- spTransform(maca.poly, soil_projection) # project MACA cell to projection of soil layer - not necessary now but will need to crop soil layer later to cut processing time for reprojection
-#soil_crop <- crop(soil, maca.poly) # crop soil raster to maca cell - same comment as above
+#park_proj <- st_transform(park, dem_projection) # reproject park
+#centroid_proj <- st_transform(centroid, dem_projection) # reproject centroid
 
-#soil_proj <- projectRaster(soil, crs = dem_projection) # project cropped soil layer to DEM projection
-maca_proj <- spTransform(maca.poly, dem_projection) # reproject MACA grid 
+soil_project_to_DEM <- projectRaster(soil_crop, crs = dem_projection) # project soil layer to DEM projection
+maca_project_to_DEM <- spTransform(maca.poly, dem_projection) # reproject MACA grid 
 
 
 #####   SLOPE, ASPECT AND RANDOM POINTS   ##########################################################################################
@@ -111,15 +115,15 @@ plot(aspect) # check aspect looks OK
 
 # Crop to projected MACA cell 
 
-soil_crop <- crop(soil, maca_proj)
-dem_crop <- crop(dem, maca_proj)
-slope_crop <- crop(slope, maca_proj)
-aspect_crop <- crop(aspect, maca_proj)
+#soil_crop <- crop(soil_proj, maca_proj)
+dem_crop <- crop(dem, maca_project_to_DEM)
+slope_crop <- crop(slope, maca_project_to_DEM)
+aspect_crop <- crop(aspect, maca_project_to_DEM)
 
-head(soil_crop@data@attributes) # check to see that RAT followed through processing
+#head(soil_crop@data@attributes) # check to see that RAT followed through processing
 
 # get 10 random points from soil raster and create SpatialPoints object
-points <- spsample(maca_proj, n = 10, type = "random")
+points <- spsample(maca_project_to_DEM, n = 10, type = "random")
 
 
 ####    PLOT TO CHECK SPATIAL ALIGNMENT   ############################################################################################
